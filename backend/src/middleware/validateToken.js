5c1d4377d4f53997d7270bf9aa0783eb4248c198
Micae1o <mrviver335@gmail.com>
2024-09-26 12:40:12 +0300

    fixed validateToken middleware

--- backend/src/middleware/validateToken.js
+++ backend/src/middleware/validateToken.js
@@ -1,4 +1,14 @@
 const axios = require('axios');
+const https = require('https');
+
+const agent = https.Agent({
+    rejectUnauthorized: false
+});
+
+const username = process.env.BASIC_AUTH_USERNAME;
+const password = process.env.BASIC_AUTH_PASSWORD;
+
+const credentials = Buffer.from(`${username}:${password}`).toString('base64');
 
 const validateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
@@ -9,18 +19,25 @@ const validateToken = async (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const response = await axios.post(`https://178.156.131.224:9000/api/auth/v1/validate_token?token=${token}`, null, {
+        const response = await axios.post(`https://178.156.131.224:9000/api/auth/v1/resource/validation`, {
+            token: token,
+            url: "/api/auth/permissions",
+            action: "GET"
+        }, {
             headers: {
-                'Accept': 'application/json'
-            }
+                'Accept': 'application/json',
+                'Authorization': `Basic ${credentials}`
+            },
+            httpsAgent: agent
         })
 
-        if (response.status === 200) {
+        if (response.data.code === 200) {
             next()
         } else {
             return res.status(422).json({ message: 'validator error' });
         }
     } catch (error) {
+        console.log(error);
         return res.status(500).json({ message: 'Failed to authenticate token' });
     }
 }
